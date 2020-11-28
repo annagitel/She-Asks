6c87710c8460a126f4dcef1a9a24e404f25c3bda
annagitel <asandler@redhat.com>
2020-11-28 19:44:11 +0200

    added activities

---
+++ app/src/main/java/com/example/sheasks/GitCategory.java
@@ -0,0 +1,14 @@
+package com.example.sheasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GitCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_category);
+    }
+}
---
+++ app/src/main/java/com/example/sheasks/PythonCategory.java
@@ -0,0 +1,14 @@
+package com.example.sheasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PythonCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_python_category);
+    }
+}
