f412a19a712c4a7e45cacaeceafef76f7dbb84d4
JessicaBlau <jblau1993@gmail.com>
2020-12-12 16:12:50 +0200

    main menu

--- app/src/main/java/com/example/sheasks/MainMenu.java
+++ app/src/main/java/com/example/sheasks/MainMenu.java
@@ -7,9 +7,5 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
-    }
+
 }
