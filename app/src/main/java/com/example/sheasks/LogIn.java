519189a40c238b4f6a54ab8d03bee0cc2f6d2e36
annagitel <asandler@redhat.com>
2020-12-13 12:31:15 +0200

    categories

--- app/src/main/java/com/example/sheasks/LogIn.java
+++ app/src/main/java/com/example/sheasks/LogIn.java
@@ -120,126 +120,8 @@ public class LogIn extends AppCompatActivity {
                 }
             });
             check_me = false;
-            //    user = FirebaseAuth.getInstance().getCurrentUser();
         }
 
-
-    }
-
-
-
-    private boolean is_ok(String email, String pass){
-
-        if(email.isEmpty() || pass.isEmpty())
-            return false;
-        else return true;
-
-    }
-}
-/*public class LogIn extends AppCompatActivity {
-
-
-    private EditText email_id , passwd_id;
-    private Button login_ ,signup_;
-    private FirebaseAuth mAuth;
-    protected User user;
-    private boolean check_me = false;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
-        mAuth = FirebaseAuth.getInstance();
-        email_id = (EditText)findViewById(R.id._email);
-        passwd_id = (EditText)findViewById(R.id._passwd);
-        login_ = (Button) findViewById(R.id._login);
-        signup_ = (Button) findViewById(R.id._register);
-
-
-        signup_.setOnClickListener(v -> {
-            if(is_ok(email_id.getText().toString().trim(),passwd_id.getText().toString().trim()))
-                register_fun(email_id.getText().toString().trim(),passwd_id.getText().toString().trim());
-        });
-
-        login_.setOnClickListener(v -> {
-            if(is_ok(email_id.getText().toString().trim(),passwd_id.getText().toString().trim()))
-
-                login_fun(email_id.getText().toString().trim(),passwd_id.getText().toString().trim());
-        });
-    }
-
-    private void login_fun(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("msg :", "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("msg :", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LogIn.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                    }
-                });
-    }
-
-
-    private void register_fun(final String email, final String pass) {
-
-        mAuth.createUserWithEmailAndPassword(email, pass)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-
-                            user = new User(email,pass);
-
-
-                            Log.d("msg :", "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            new FirebaseHelper().addUser(user);
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("msg :", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(LogIn.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-
-        if(user != null){
-            new FirebaseHelper().readUser(new FirebaseHelper.OnGetDataListener() {
-                @Override
-                public void onSuccess(User usr) {
-                    if(!usr.getAdmin() && !check_me) {
-                        startActivity(new Intent(LogIn.this, MainActivity.class));
-                        check_me = true;
-                    }
-                    else if(usr.getAdmin() && !check_me) {
-                        startActivity(new Intent(LogIn.this, MainActivity.class));
-                        check_me = true;
-                    }
-                }
-
-            });
-            check_me = false;
-            //    user = FirebaseAuth.getInstance().getCurrentUser();
-        }
 
     }
 
@@ -250,4 +132,4 @@ public class LogIn extends AppCompatActivity {
         else return true;
 
     }
-}*/
+}
